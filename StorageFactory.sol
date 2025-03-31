3b61e58e44053f4fd63628334357884f6114de5d
DEVHYDAR01 <aliyuahmaad60@gmail.com>
2025-03-31 13:27:44 +0100

    This commit will push storagefactory.sol

--- StorageFactory.sol
+++ StorageFactory.sol
@@ -1,37 +1,24 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
-contract SimpleStorage {
-    uint256 favoriteNumber;
-
-    struct Person {
-        uint256 favoritenumber;
-        string name;
-    }
-
-    Person[] public list_of_people;
-
-    mapping(string => uint256) public nameToFavoriteNumber;
-
-    function add_person(string memory _name, uint256 _favoritenumber) public {
-        list_of_people.push(Person(_favoritenumber, _name));
-        nameToFavoriteNumber[_name] = _favoritenumber;
-    }
-
-    function store(uint256 _favoriteNumber) public {
-        favoriteNumber = _favoriteNumber;
-    }
-
-    function retrieve() public view returns (uint256) {
-        return favoriteNumber;       
-    }
-}
+import { SimpleStorage } from "./SimpleStorage.sol";
 
 contract StorageFactory {
-    SimpleStorage public simpleStorage;
+    SimpleStorage[] public listOfsimpleStorage;
 
     function createSimpleStorage() public {
-        simpleStorage = new SimpleStorage();
+        SimpleStorage myNewSimpleStorage = new SimpleStorage();
+        listOfsimpleStorage.push(myNewSimpleStorage);
     }
 
+    function sFStore(uint256 _storeindex, uint256 _storeFavNumber) public {
+        //address
+        //ABI - Application Binary Interface
+        SimpleStorage mySimpleStorage = listOfsimpleStorage[_storeindex];
+        mySimpleStorage.store(_storeFavNumber);
+    }
+    function sfGet(uint256 _storeindex) public view returns (uint256) {
+        // return SimpleStorage(address(simpleStorageArray[_simpleStorageIndex])).retrieve();
+        return listOfsimpleStorage[_storeindex].retrieve();
+    }
 }
